6e7b0a956c57c3f5cd47a388e01346e850efd5c1
Waewprach <TidjunG@tidjungs.local>
2016-05-01 20:35:24 +0700

    pass data into components

--- src/App.js
+++ src/App.js
@@ -2,16 +2,21 @@ import React from 'react'
 import ReactDOM from 'react-dom';
 import ContactsList from './ContactsList';
 
+let contacts = {
+    name: 'Tidjungsssss',
+    phone: '111 222 333'
+}
+
 class App extends React.Component {
     render() {
         return (
             <div>
-                <h1>Tidjungs</h1>
-                <ContactsList />
+                <h1> Contacts List </h1>
+                <ContactsList contacts={this.props.contacts} />
             </div>      
         )
     }
 }
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<App contacts={contacts}/>, document.getElementById('app'));
 
--- src/ContactsList.js
+++ src/ContactsList.js
@@ -4,7 +4,7 @@ class ContactsList extends React.Component {
     render() {
         return (
             <ul>
-                <li>Waewprach 111 222 333</li>
+                <li>{this.props.contacts.name} {this.props.contacts.phone}</li>
             </ul>
         )
     }
